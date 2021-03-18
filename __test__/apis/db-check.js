4febca17f80a97d2403bbb8dc1918002e7ff4938
liumingzhou <18231133236@163.com>
2021-03-18 13:37:10 +0800

    test again and again

--- __test__/apis/db-check.js
+++ __test__/apis/db-check.js
@@ -9,8 +9,8 @@ test('数据库连接', async () => {
     const { data, errno } = await get('/api/db-check')
     const { redisConn, mysqlConn, mongodbConn } = data
 
-    expect(redisConn).toBe(true)
     expect(errno).toBe(0)
+    expect(redisConn).toBe(true)
     expect(mysqlConn).toBe(true)
     expect(mongodbConn).toBe(true)
 })
